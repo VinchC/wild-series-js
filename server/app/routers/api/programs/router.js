e07b6bd6852ed608050f2d0a78ae2454f3d949d7
Vincent C <vinch.cm@gmail.com>
2024-05-21 12:32:11 +0200

    quest 3 Harmonia done

---
+++ server/app/routers/api/programs/router.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { browse } = require("../../../controllers/programActions");
+
+router.get("/", browse);
+
+/* ************************************************************************* */
+
+module.exports = router;
--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,13 +2,19 @@ const express = require("express");
 
 const router = express.Router();
 
-/* ************************************************************************* */
+const { welcome } = require("../../controllers/sayActions");
+
 // Import And Use Routers Here
-/* ************************************************************************* */
+
+router.get("/", welcome);
 
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
+
+const programsRouter = require("./programs/router");
+
+router.use("/programs", programsRouter);
 
 /* ************************************************************************* */
 
